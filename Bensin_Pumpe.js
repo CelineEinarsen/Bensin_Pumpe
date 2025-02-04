22b15fcda85377af5d43a2db239604949b09adda
Celine 3 <cel13012@skole.telemarkfylke.no>
2025-02-04 09:00:29 +0100

    Create Bensin_Pumpe.js

---
+++ Bensin_Pumpe.js
@@ -0,0 +1,24 @@
+window.onload = oppstart;
+
+function oppstart(){
+    document.querySelector("#bensin").onclick = LiterRegning;
+
+}
+
+function LiterRegning(){
+    let Liter = (document.querySelector("#tall").value);
+    const Pris = 22;
+    let Sum = Liter * Pris;
+
+    if (isNaN(Liter)){
+        document.querySelector("#utskrift").innerHTML = "Vennligst skriv inn et tall";
+
+    }
+
+    else if(Sum){
+        document.querySelector("#utskrift").innerHTML = "Du må betale " + Sum + "kr, for " + Liter + " liter bensin.";
+
+    }
+
+}
+LiterRegning();
